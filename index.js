5a8318e4f19f4395c4bba84478d25e207903c3e0
Anegelisai <63125934+Anegelisai@users.noreply.github.com>
2021-01-10 19:56:54 -0600

    config express

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
+// npm init -y 
+// npm i nodemon -g
+// npm i express
 
 
+const express = require('express');
 
-console.log("hola mundno");
+// Crear el servidor de express}
+
+const app = express();
+
+// rutas
+
+app.get("/",(req,res)=>{
+    console.log("se requiere el /");
+    res.json({
+        ok:true
+    })
+})
+
+// ecuchar peticiones
+
+app.listen(4000, ()=>{
+    console.log(`servidor corriendo en puerto ${4000}`);
+
+});
