d48cc38a83f94aeae24b451289c27c28691804e7
Anegelisai <63125934+Anegelisai@users.noreply.github.com>
2021-01-10 21:23:49 -0600

    env and public(index)

--- index.js
+++ index.js
@@ -1,26 +1,34 @@
 // npm init -y 
 // npm i nodemon -g
 // npm i express
-
+// vareables de entorno    npm i dotenv
 
 const express = require('express');
+require("dotenv").config();
+
 
 // Crear el servidor de express}
 
 const app = express();
 
+
+// directorio publico
+
+app.use(express.static("public"));
+
 // rutas
 
-app.get("/",(req,res)=>{
-    console.log("se requiere el /");
-    res.json({
-        ok:true
-    })
-})
+// app.get("/",(req,res)=>{
+//     console.log("se requiere el /");
+//     res.json({
+//         ok:true
+//     })
+// });
+
 
 // ecuchar peticiones
 
-app.listen(4000, ()=>{
+app.listen(process.env.PORT, ()=>{
     console.log(`servidor corriendo en puerto ${4000}`);
 
 });
