3a501552149ec6e5ac7021746ccc3ebf9bb57ce6
Anegelisai <63125934+Anegelisai@users.noreply.github.com>
2021-01-11 21:56:29 -0600

    express validator middlewares

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,18 +1,22 @@
 const {response} = require('express');
+const { validationResult } = require('express-validator');
 
 
 const crearUsuario = (req , res = response)=>{ 
 
     const {name, email, password} = req.body;
 
-    if(name.length < 5 ){
+    // manejo de errores
+    const errors = validationResult(req) ;
+    if(!errors.isEmpty()){
         return res.status(400).json({
             ok:false,
-            msg:"el name debe ser de minimo 5 letras"
+            errors: errors.mapped()
         });
     }
 
-    return res.json({
+
+    return res.status(201).json({
         ok:true,
         msg:"register",
         name,
@@ -23,7 +27,16 @@ const crearUsuario = (req , res = response)=>{
 
 const login = (req,res=response)=>{
     const {email, password} = req.body;
-    res.json({
+
+    const errors = validationResult(req) ;
+    if(!errors.isEmpty()){
+        return res.status(400).json({
+            ok:false,
+            errors: errors.mapped()
+        });
+    }
+
+    return res.status(201).json({
         ok:true,
         msg:"login",
         email,
--- routes/auth.js
+++ routes/auth.js
@@ -4,15 +4,30 @@
     host + /api/auth
 */
 const {Router} = require('express');
+const { check } = require('express-validator');
 const router = Router();
 const {crearUsuario, login, renew} = require('../controllers/auth')
 
 
 
-router.post("/new",crearUsuario);
+router.post(
+    "/new",
+    [ //middlewares
+        check("name","el nombre es obligatorio").not().isEmpty(),
+        check("email","el email es obligatorio").isEmail(),
+        check("password","el password es obligatorio").not().isEmpty(),
+        check("password","el password debe ser mayor de 6 caracteres").isLength({min:6}),
+    ],
+    crearUsuario);
 
 
-router.post("/",login);
+router.post(
+    "/",
+    [
+        check("email","el email es obligatorio").isEmail(),
+        check("password","el password debe ser mayor de 6 caracteres").isLength({min:6}),
+    ],
+    login);
 
 
 router.get("/renew",renew);
