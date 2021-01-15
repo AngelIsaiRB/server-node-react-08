56939daa2081c67d24eaf8f17390bc9c7f41819c
Anegelisai <63125934+Anegelisai@users.noreply.github.com>
2021-01-14 20:46:02 -0600

    DB add new user

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,18 +1,25 @@
 const {response} = require('express');
 const { validationResult } = require('express-validator');
+const User = require('../models/User');
 
 
-const crearUsuario = (req , res = response)=>{ 
+const crearUsuario = async (req , res = response)=>{ 
 
-    const {name, email, password} = req.body;
-
-    return res.status(201).json({
+    try {
+        const {name, email, password} = req.body;
+        const usuario = new User(req.body);
+        await usuario.save();
+        return res.status(201).json({
         ok:true,
         msg:"register",
-        name,
-        email,
-        password
-    })
+        })
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            ok:false,
+            msg:"error interno hable con el admin",
+        })
+    }
 }
 
 const login = (req,res=response)=>{
