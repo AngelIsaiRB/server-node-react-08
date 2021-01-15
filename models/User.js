b378081ce734e3e71c979a3b5eeccf81d6dbc123
Anegelisai <63125934+Anegelisai@users.noreply.github.com>
2021-01-14 21:32:43 -0600

    add models

---
+++ models/User.js
@@ -0,0 +1,22 @@
+const {Schema, model} = require('mongoose');
+
+const UserSchema = Schema({
+
+    name:{
+        type: 'string',
+        required: true,
+    },
+    email:{
+        type: 'string',
+        required: true,
+        unique: true,
+    },
+    password:{
+        type: 'string',
+        required: true,        
+    }
+
+});
+
+
+module.exports = model ("User",UserSchema);
